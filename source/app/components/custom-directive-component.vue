12adb322a2443a3a6aeb5f6edfc7fade13564754
Jhon Navarrete <JhonErik15769@hotmail.com>
2023-01-21 20:01:07 -0500

    The Directive Component was created.

---
+++ source/app/components/custom-directive-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Custom Directives Vue</h2>
+  <p>As you know, the <a target="_blank" rel="noopener noreferrer" href="https://vuejs.org/api/built-in-directives.html#v-html"><code>v-html</code></a> directive updates the element's innerHTML. In some occasions it will be necessary to update the OuterHTML of the document, so we will create a directive to do it.</p>
+  <p></p>
+  <div v-outer-html="{ color: 'blue', text: '2023' }"></div>
+</template>
+
+<script>
+export default {
+    name: 'CustomDirectiveComponent',
+    data() {
+      return {
+      };
+    },
+}
+</script>
+
+<style>
+
+</style>
